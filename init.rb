cafe3e5c52af2e8937d9013af04414d23b190bf1
Will Leinweber <will@bitfission.com>
2014-07-25 16:20:21 -0700

    log every time you have to press yubikey

--- init.rb
+++ init.rb
@@ -1,3 +1,4 @@
+require 'fileutils'
 class Heroku::Auth
   class << self
     alias_method :_orig_ask_for_second_factor, :ask_for_second_factor
@@ -5,6 +6,9 @@ class Heroku::Auth
       `osascript -e 'tell application "yubiswitch" to KeyOn'`
       result = _orig_ask_for_second_factor
       `osascript -e 'tell application "yubiswitch" to KeyOff'`
+      file = File.expand_path('~/.heroku/yubipresses.log')
+      FileUtils.touch file
+      open(file, 'a') { |f| f.puts Time.now.utc.to_s }
       result
     end
   end
